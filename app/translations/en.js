a3a6d29307582985ef3eac557a2787f251940459
João Alves <joao.cpp.sca@gmail.com>
2015-12-06 19:10:03 +0000

    User login style update

--- app/translations/en.js
+++ app/translations/en.js
@@ -8,9 +8,10 @@ app.config(function ($translateProvider) {
 		CREATE_NEW_ACCOUNT: 'Create new Account',
 		FORGOT_PASSWORD: 'Forgot your password?',
 		LOGIN: 'Login',
+		REGISTER: 'Register',
 		RETURN_LOGIN: 'Return to Login',
 		PLACEHOLDER_LOGIN_IDENTITY: 'Your email or username',
-		PLACEHOLDER_LOGIN_EMAIL: 'Your email',
+		PLACEHOLDER_LOGIN_EMAIL: 'Your email address',
 		PLACEHOLDER_LOGIN_USERNAME: 'Your username',
 		PLACEHOLDER_LOGIN_PASSWORD: 'Your password',
 		PLACEHOLDER_LOGIN_CONFIRM_PASSWORD: 'Confirm your password'
